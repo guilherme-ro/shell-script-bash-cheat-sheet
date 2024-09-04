d30a675898481bdbfd65309c39ca42f7470dd647
Guilherme Ramos <guilherme.ramos09@gmail.com>
2024-09-04 14:54:06 -0300

    add more commands

--- bash.sh
+++ bash.sh
@@ -21,13 +21,33 @@ then
 fi
 
 # IF-ELSE
-
+if [ "$NAME" == "Brad" ]
+then
+    echo "Yuur name is Brad"
+else
+    echo "Your name is NOT Brad"
+fi
 
 # ELSE-IF (elif)
-
+if [ "NAME" == "Brad"]
+then
+    echo "Your name is Brad"
+elif [ "$NAME" == "Jack" ]
+then
+    echo "Your name is Jack"
+else
+    echo "Your name is NOT Brad or Jack"
+fi
 
 # COMPARISON
-
+NUM1=31
+NUM2=5
+if [ "$NUM1" -gt "$NUM2" ]
+then
+    echo "$NUM1 is greater than $NUM2"
+else 
+    echo "$NUM1 is less than $NUM2"
+fi 
 
 ########
 # val1 -eq val2 Returns true if the values are equal
@@ -39,7 +59,13 @@ fi
 ########
 
 # FILE CONDITIONS
-
+FILE="test.txt"
+if [ -e "$FILE" ]
+then
+    echo "$FILE exists"
+else
+    echo "$FILE does NOT exist"
+fi 
 
 ########
 # -d file   True if the file is a directory
@@ -54,23 +80,58 @@ fi
 ########
 
 #CASE STATEMENT
-
+read -p "Are you 21 or over? Y/N " $ANSWER
+case "$ANSWER" in
+    [yY] | [yY][eE][sS]
+        echo "You have a beer :)"
+        ;;
+    [nN] | [nN][oO]
+        echo "Sorry, no drinking"
+        ;;
+    *)
+        echo "Please enter y/yes or n/no"
+    ;;
+esac
 
 # SIMPLE FOR LOOP
-
+NAMES="Brad Kevin Alice Mark"
+for NAME in $NAMES
+  do
+    echo "Hello $NAME"
+done
 
 # FOR LOOP TO RENAME FILES
-
+FILES=$(ls *.txt)
+NEW="new"
+for FILE in $FILES  
+  do
+    echo "Renaming $FILE to new-$FILE"
+    mv $FILE $NEW-$FILE
+done
 
 # WHILE LOOP - READ THROUGH A FILE LINE BY LINE
-
+LINE=1
+while read -r CURRENT_LINE
+  do
+    echo "$LINE: $CURRENT_LINE"
+    ((LINE++))
+done < "./new-1.txt"
 
 # FUNCTION
-
+function sayHello() {
+  echo "Hello World"
+}
+sayHello
 
 # FUNCTION WITH PARAMS
+function greet() {
+  echo "Hello, I am $1 and I am $2"
+}
 
-
-# greet "Brad" "36"
+greet "Brad" "36"
 
 # CREATE FOLDER AND WRITE TO A FILE
+mkdir hello
+touch "hello/world.txt"
+echo "Hello World" >> "hello/world.txt"
+echo "Created hello/world.txt"
